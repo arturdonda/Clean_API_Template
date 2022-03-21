1c05a84975f801f9697c79edb8dee25b97351112
Artur Donda <artur.donda@outlook.com>
2022-03-21 01:50:08 -0300

    Changing Error name property

--- src/domain/Errors/InvalidParamError.ts
+++ src/domain/Errors/InvalidParamError.ts
@@ -2,6 +2,7 @@ export class InvalidParamError extends Error {
 	constructor(param: string, message: string) {
 		super(`Campo '${param}' inválido: ${message}`);
 
+		this.name = 'InvalidParamError';
 		Object.setPrototypeOf(this, InvalidParamError.prototype);
 	}
 }
