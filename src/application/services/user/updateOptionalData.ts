f26a43480e1e1048ae675249134c8b8dbc2b102f
Artur Donda <artur.donda@outlook.com>
2022-03-22 16:02:39 -0300

    Adding updateOptionalData usecase implementation

---
+++ src/application/services/user/updateOptionalData.ts
@@ -0,0 +1,22 @@
+import { IUpdateOptionalData } from '@/domain/usecases/user';
+import { UserNotFoundError } from '@/application/protocols/errors';
+import { IUserRepository } from '@/application/protocols/repository';
+
+export class UpdateOptionalData implements IUpdateOptionalData {
+	constructor(private readonly userRepository: IUserRepository) {}
+
+	exec = async ({ userId, address, birthday, cpf, gender, phone, rg }: IUpdateOptionalData.Params): Promise<IUpdateOptionalData.Result> => {
+		const user = await this.userRepository.getById(userId);
+
+		if (!user) throw new UserNotFoundError();
+
+		if (address) user.address = address;
+		if (birthday) user.birthday = birthday;
+		if (cpf) user.cpf = cpf;
+		if (gender) user.gender = gender as any;
+		if (phone) user.phone = phone;
+		if (rg) user.rg = rg;
+
+		return await this.userRepository.update(user);
+	};
+}
