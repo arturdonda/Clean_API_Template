9d2f9ea1e3a679173fa3007a3bf11a67b1ba59e3
Artur Donda <artur.donda@outlook.com>
2022-03-23 15:01:42 -0300

    Adding crypto

---
+++ src/infra/adapters/uuid/crypto/uuidService.ts
@@ -0,0 +1,8 @@
+import { IUuidService } from '@/application/protocols/utils';
+import { randomUUID } from 'crypto';
+
+export class UuidService implements IUuidService {
+	generate = (seed?: string): string => {
+		return randomUUID().toString().replace(/-/g, '');
+	};
+}
