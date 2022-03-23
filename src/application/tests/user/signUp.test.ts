1ecc739ffc521afc568a0a0d30e8f8011398a7eb
Artur Donda <artur.donda@outlook.com>
2022-03-22 23:45:02 -0300

    SignUp usecase test

---
+++ src/application/tests/user/signUp.test.ts
@@ -0,0 +1,61 @@
+import { MockHashService, MockIpService, MockTokenService, MockUserRepository, MockUuidService } from '@/application/tests/mock';
+import { SignIn, SignUp } from '@/application/services/user';
+import { CreateSession, RenewAccess } from '@/application/services/session';
+import { InvalidPasswordError, UserAccountPendingActivation, UserNotFoundError, UserRegisteredError } from '@/application/protocols/errors';
+
+describe('Sign Up', () => {
+	const userRepository = new MockUserRepository();
+	const uuidService = new MockUuidService();
+	const passwordHashService = new MockHashService();
+	const tokenService = new MockTokenService();
+	const ipService = new MockIpService();
+	const createSessionService = new CreateSession(tokenService, ipService);
+	const renewAccessService = new RenewAccess(tokenService);
+	const signUpService = new SignUp(userRepository, uuidService, passwordHashService);
+	const signInService = new SignIn(userRepository, passwordHashService, createSessionService, renewAccessService);
+
+	test('Valid parameters', async () => {
+		expect(
+			await signUpService.exec({
+				name: 'John Doe',
+				email: 'john.doe@hotmail.com',
+				password: 'Abcde#123',
+				confirmationPassword: 'Abcde#123',
+			})
+		).resolves;
+
+		expect(
+			signInService.exec({
+				email: 'john.doe@hotmail.com',
+				password: 'Abcde#123',
+				ipAddress: '0.0.0.0',
+			})
+		).rejects.toThrow(UserAccountPendingActivation);
+	});
+
+	test('Registered e-mail', async () => {
+		const user = await userRepository.getById('1');
+
+		if (!user) throw new UserNotFoundError();
+
+		expect(
+			signUpService.exec({
+				name: 'John Doe',
+				email: user.email,
+				password: 'Abcde#123',
+				confirmationPassword: 'Abcde#123',
+			})
+		).rejects.toThrow(UserRegisteredError);
+	});
+
+	test('Unmatch passwords', async () => {
+		expect(
+			signUpService.exec({
+				name: 'John Doe',
+				email: 'john.doe@hotmail.com',
+				password: 'Abcde#123',
+				confirmationPassword: 'Abcd#123',
+			})
+		).rejects.toThrow(InvalidPasswordError);
+	});
+});
