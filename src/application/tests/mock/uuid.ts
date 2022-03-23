7488ca3783f159638163aa35b38e0144ea82c957
Artur Donda <artur.donda@outlook.com>
2022-03-22 23:44:28 -0300

    Fixing MockUuidService's class name

--- src/application/tests/mock/uuid.ts
+++ src/application/tests/mock/uuid.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'crypto';
 import { IUuidService } from '@/application/protocols/utils';
 
-export class UuidService implements IUuidService {
+export class MockUuidService implements IUuidService {
 	generate = (seed?: string): string => {
 		return randomUUID().toString().replace(/-/g, '');
 	};
