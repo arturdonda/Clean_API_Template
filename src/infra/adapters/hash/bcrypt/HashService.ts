1046ab5a0556b88d16716f9c2fe733f796067584
Artur Donda <artur.donda@outlook.com>
2022-03-23 14:27:53 -0300

    Adding bcrypt

---
+++ src/infra/adapters/hash/bcrypt/HashService.ts
@@ -0,0 +1,14 @@
+import { IHashService } from '@/application/protocols/utils';
+import bcrypt from 'bcryptjs';
+
+export class HashService implements IHashService {
+	hash = (password: string): string => {
+		const salt = bcrypt.genSaltSync();
+
+		return bcrypt.hashSync(password, salt);
+	};
+
+	verify = (password: string, hash: string): boolean => {
+		return bcrypt.compareSync(password, hash);
+	};
+}
