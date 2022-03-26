33ce58fb824aa8fd32cbca1f4833f03104b3fd82
Artur Donda <artur.donda@outlook.com>
2022-03-26 17:55:15 -0300

    RenewAccess controller

---
+++ src/presentation/controllers/session/renewAccess.ts
@@ -0,0 +1,20 @@
+import { IRenewAccess } from '@/domain/usecases/session';
+import { Controller, HttpRequest, HttpResponse, ok, internalServerError } from '@/presentation/protocols';
+
+export class RenewAccessController implements Controller {
+	constructor(private readonly renewAccessService: IRenewAccess) {}
+
+	handle = async (request: HttpRequest): Promise<HttpResponse<null>> => {
+		try {
+			const accessToken = this.renewAccessService.exec(request.cookies.refreshToken);
+
+			return ok({
+				message: 'Acesso renovado com sucesso',
+				result: null,
+				headers: { authorization: `Bearer ${accessToken}` },
+			});
+		} catch (error) {
+			return internalServerError({ message: 'Erro ao renovar acesso', result: error });
+		}
+	};
+}
