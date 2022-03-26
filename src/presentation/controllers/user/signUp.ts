63d66a4b9066dbb66af1866465a40ddb7c644584
Artur Donda <artur.donda@outlook.com>
2022-03-26 18:01:05 -0300

    SignUp Controller

---
+++ src/presentation/controllers/user/signUp.ts
@@ -0,0 +1,28 @@
+import { ISignUp } from '@/domain/usecases/user';
+import { UserViewModel } from '@/presentation/viewModels';
+import { MissingParamError } from '@/presentation/errors';
+import { Controller, HttpRequest, HttpResponse, created, badRequest, internalServerError } from '@/presentation/protocols';
+
+export class SignUpController implements Controller {
+	constructor(private readonly signUpService: ISignUp) {}
+
+	handle = async (request: HttpRequest): Promise<HttpResponse<UserViewModel>> => {
+		try {
+			if (!request.body.name) throw new MissingParamError('name');
+			if (!request.body.email) throw new MissingParamError('email');
+			if (!request.body.password) throw new MissingParamError('password');
+			if (!request.body.confirmationPassword) throw new MissingParamError('confirmationPassword');
+
+			const user = await this.signUpService.exec({
+				name: request.body.name,
+				email: request.body.email,
+				password: request.body.password,
+				confirmationPassword: request.body.confirmationPassword,
+			});
+
+			return created({ message: 'Usuário criado com sucesso.', result: UserViewModel.map(user) });
+		} catch (error) {
+			return (error instanceof MissingParamError ? badRequest : internalServerError)({ message: 'Erro ao criar usuário.', result: error });
+		}
+	};
+}
