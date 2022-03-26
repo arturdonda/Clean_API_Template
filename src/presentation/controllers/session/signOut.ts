961c10a71b98ec3269db21e25272697c0a56b2c7
Artur Donda <artur.donda@outlook.com>
2022-03-26 17:58:57 -0300

    SignOut controller

---
+++ src/presentation/controllers/session/signOut.ts
@@ -0,0 +1,16 @@
+import { IRevokeSession } from '@/domain/usecases/session';
+import { Controller, HttpRequest, HttpResponse, ok, internalServerError } from '@/presentation/protocols';
+
+export class SignOutController implements Controller {
+	constructor(private readonly revokeSessionService: IRevokeSession) {}
+
+	handle = async (request: HttpRequest): Promise<HttpResponse<null>> => {
+		try {
+			await this.revokeSessionService.exec({ sessionToken: request.cookies.refreshToken, ipAddress: request.ip });
+
+			return ok({ message: 'Sessão finalizada com sucesso', result: null });
+		} catch (error) {
+			return internalServerError({ message: 'Erro ao finalizar sessão', result: error });
+		}
+	};
+}
