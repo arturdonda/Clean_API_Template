cdeaf5f1ac3d43b524afd22341823433cfcb0e7f
Artur Donda <artur.donda@outlook.com>
2022-03-27 12:54:27 -0300

    Express route adapter

---
+++ src/main/adapters/expressRoutes.ts
@@ -0,0 +1,24 @@
+import { Controller, HttpRequest } from '@presentation/protocols';
+import { Request, Response, NextFunction } from 'express';
+
+export const adaptRoute = (controller: Controller) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		const httpRequest = makeHttpRequest(req);
+		const httpResponse = await controller.handle(httpRequest);
+
+		next(httpResponse);
+	};
+};
+
+const makeHttpRequest = (req: Request): HttpRequest => {
+	const request: HttpRequest = {
+		ip: req.ip,
+		query: req.params,
+		headers: req.headers as Record<string, string>,
+		cookies: req.cookies,
+		body: req.body,
+		userId: req.userId,
+	};
+
+	return request;
+};
