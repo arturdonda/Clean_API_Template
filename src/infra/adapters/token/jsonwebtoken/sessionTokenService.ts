04aca94c0c55cd5d036c8b1212ba40d30f201a03
Artur Donda <artur.donda@outlook.com>
2022-03-27 13:08:48 -0300

    Rename env vars from refreshToken to sessionToken

--- src/infra/adapters/token/jsonwebtoken/sessionTokenService.ts
+++ src/infra/adapters/token/jsonwebtoken/sessionTokenService.ts
@@ -2,8 +2,8 @@ import { ITokenService } from '@application/protocols/utils';
 import { JwtAdapter } from './base';
 
 export class SessionTokenService implements ITokenService {
-	private readonly _secret = process.env.REFRESH_TOKEN_SECRET;
-	private readonly _expiresIn = `${process.env.REFRESH_TOKEN_EXPIRATION_IN_DAYS}d`;
+	private readonly _secret = process.env.SESSION_TOKEN_SECRET;
+	private readonly _expiresIn = `${process.env.SESSION_TOKEN_EXPIRATION_IN_DAYS}d`;
 
 	private readonly _jwtAdapter = new JwtAdapter(this._secret, this._expiresIn);
 
