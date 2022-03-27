eedd56f8370cdef8e32731cbe6c58dbe065fe19b
Artur Donda <artur.donda@outlook.com>
2022-03-27 12:55:04 -0300

    Authorize middleware

---
+++ src/main/middlewares/authorize.ts
@@ -0,0 +1,17 @@
+import { AccessTokenService } from '@infra/adapters/token/jsonwebtoken';
+import { unauthorized } from '@presentation/protocols';
+import { Request, Response, NextFunction } from 'express';
+
+export const authorize = async (req: Request, res: Response, next: NextFunction) => {
+	if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))
+		return next(unauthorized({ message: 'Você deve fazer o login antes.', result: null }));
+
+	const accessTokenService = new AccessTokenService();
+
+	const accessToken = accessTokenService.validate(req.headers.authorization.split(' ')[1]);
+
+	if (accessToken.expiredAt >= new Date()) return next(unauthorized({ message: 'Token de acesso expirado', result: null }));
+
+	req.userId = accessToken.audience;
+	return next();
+};
