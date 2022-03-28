ad8fc777b387a31c5dea009059551e82e9f103d0
Artur Donda <artur.donda@outlook.com>
2022-03-28 13:32:03 -0300

    Adding user routes

---
+++ src/main/routes/user.ts
@@ -0,0 +1,33 @@
+import { adaptRoute } from '@main/adapters';
+import { authorize } from '@main/middlewares';
+import {
+	makeGetActiveSessionsController,
+	makeGetAllUsersController,
+	makeGetUserByIdController,
+	makeGetUserMeController,
+	makeSignUpController,
+	makeUpdateOptionalDataController,
+	makeUpdatePasswordController,
+} from '@main/factories/user';
+import { Router } from 'express';
+import { makeRevokeSessionController } from '@main/factories/session';
+
+export default (router: Router): void => {
+	const prefix = 'user';
+
+	router.get(`/${prefix}/all`, authorize, adaptRoute(makeGetAllUsersController()));
+
+	router.get(`/${prefix}/:userId`, authorize, adaptRoute(makeGetUserByIdController()));
+
+	router.get(`/${prefix}/me`, authorize, adaptRoute(makeGetUserMeController()));
+
+	router.get(`/${prefix}/session`, authorize, adaptRoute(makeGetActiveSessionsController()));
+
+	router.post(`/${prefix}`, adaptRoute(makeSignUpController()));
+
+	router.put(`/${prefix}/me`, authorize, adaptRoute(makeUpdateOptionalDataController()));
+
+	router.put(`/${prefix}/me/password`, authorize, adaptRoute(makeUpdatePasswordController()));
+
+	router.delete(`/${prefix}/session/:sessionToken`, authorize, adaptRoute(makeRevokeSessionController()));
+};
