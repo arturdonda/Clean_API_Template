81ba78d1383cca526e8b589929c1268375ffa9af
Artur Donda <artur.donda@outlook.com>
2022-03-28 13:30:20 -0300

    Test for Update Password Controller

---
+++ src/presentation/__tests__/user/updatePassword.test.ts
@@ -0,0 +1,83 @@
+import { UpdatePassword } from '@application/services/user';
+import { MockHashService, MockUserRepository } from '@application/__tests__/mock';
+import { UpdatePasswordController } from '@presentation/controllers/user';
+import { MissingParamError } from '@presentation/errors';
+
+describe('Update Password Controller', () => {
+	const mockUserRepository = new MockUserRepository();
+	const passwordHashService = new MockHashService();
+	const updatePasswordService = new UpdatePassword(mockUserRepository, passwordHashService);
+	const updatePasswordController = new UpdatePasswordController(updatePasswordService);
+
+	test('With required parameter', async () => {
+		const serviceSpy = jest.spyOn(updatePasswordService, 'exec');
+
+		expect(
+			await updatePasswordController.handle({
+				ip: '0.0.0.0',
+				userId: '1',
+				query: null,
+				cookies: null,
+				headers: null,
+				body: { password: 'John@123', confirmationPassword: 'John@123' },
+			})
+		).toMatchObject(
+			expect.objectContaining({
+				statusCode: 200,
+				body: {
+					success: true,
+					message: 'Senha atualizada com sucesso.',
+					result: null,
+				},
+			})
+		);
+
+		expect(serviceSpy).toHaveBeenCalledTimes(1);
+
+		serviceSpy.mockRestore();
+	});
+
+	test('Without required parameter - password', async () => {
+		expect(
+			updatePasswordController.handle({
+				ip: '0.0.0.0',
+				userId: '',
+				query: null,
+				cookies: null,
+				headers: null,
+				body: { confirmationPassword: 'John@123' },
+			})
+		).resolves.toMatchObject(
+			expect.objectContaining({
+				statusCode: 400,
+				body: {
+					success: false,
+					message: 'Erro ao atualizar senha.',
+					result: expect.any(MissingParamError),
+				},
+			})
+		);
+	});
+
+	test('Without required parameter - confirmationPassword', async () => {
+		expect(
+			updatePasswordController.handle({
+				ip: '0.0.0.0',
+				userId: '',
+				query: null,
+				cookies: null,
+				headers: null,
+				body: { password: 'John@123' },
+			})
+		).resolves.toMatchObject(
+			expect.objectContaining({
+				statusCode: 400,
+				body: {
+					success: false,
+					message: 'Erro ao atualizar senha.',
+					result: expect.any(MissingParamError),
+				},
+			})
+		);
+	});
+});
