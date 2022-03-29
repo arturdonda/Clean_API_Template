8ac499f51f1495185d2faca464afca104ba752e3
Artur Donda <artur.donda@outlook.com>
2022-03-28 21:34:32 -0300

    Moving required imports to express config file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,3 @@
-require('dotenv').config();
-import 'module-alias/register';
 import app from '@main/config/express';
 
 app.listen(process.env.PORT, () => console.log(`[Server]: ⚡️ Running on port ${process.env.PORT}`));
