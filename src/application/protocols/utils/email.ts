3f9648817edfde6687e2819f1a8519664bdf7732
Artur Donda <artur.donda@outlook.com>
2022-04-09 15:24:12 -0300

    Email service sends different types of email

--- src/application/protocols/utils/email.ts
+++ src/application/protocols/utils/email.ts
@@ -1,8 +1,14 @@
 export interface IEmailService {
-	send: ({ to, cc, bcc, subject, body }: IEmailService.Params) => Promise<IEmailService.Result>;
+	send: ({ to, cc, bcc, subject, body }: IEmailService.SendParams) => Promise<IEmailService.Result>;
+	sendAccountConfirmationEmail: ({ name, email, confirmationCode }: IEmailService.AccountConfirmationParams) => Promise<IEmailService.Result>;
+	sendForgotPasswordEmail: ({ name, email, resetToken }: IEmailService.ForgotPasswordParams) => Promise<IEmailService.Result>;
+	sendPasswordChangeConfirmationEmail: ({ name, email }: IEmailService.PasswordChangeConfirmationParams) => Promise<IEmailService.Result>;
 }
 
 export namespace IEmailService {
-	export type Params = { to: string[]; cc: string[]; bcc: string[]; subject: string; body: string };
+	export type SendParams = { to: string[] | string; cc?: string[] | string; bcc?: string[] | string; subject: string; body: string };
+	export type AccountConfirmationParams = { name: string; email: string; confirmationCode: string };
+	export type ForgotPasswordParams = { name: string; email: string; resetToken: string };
+	export type PasswordChangeConfirmationParams = { name: string; email: string };
 	export type Result = void;
 }
--- src/infra/adapters/email/nodemailer/emailService.ts
+++ src/infra/adapters/email/nodemailer/emailService.ts
@@ -13,7 +13,8 @@ export class EmailService implements IEmailService {
 			},
 		});
 	}
-	send = async ({ to, cc, bcc, subject, body }: IEmailService.Params): Promise<IEmailService.Result> => {
+
+	send = async ({ to, cc, bcc, subject, body }: IEmailService.SendParams): Promise<IEmailService.Result> => {
 		await this._transport.sendMail({
 			from: `"API Template" <${process.env.EMAIL_USERNAME}>`,
 			to: to,
@@ -21,6 +22,47 @@ export class EmailService implements IEmailService {
 			bcc: bcc,
 			subject: subject,
 			html: body,
+		});
+	};
+
+	sendAccountConfirmationEmail = async ({ name, email, confirmationCode }: IEmailService.AccountConfirmationParams): Promise<void> => {
+		await this.send({
+			to: email,
+			subject: 'Confirme a sua conta',
+			body: `<p>Olá, ${name}!</p>
+			<br>
+			<p>Sua conta no API TEMPLATE está quase pronta. Para ativá-la, por favor confirme o seu endereço de email clicando no link abaixo.</p>
+			<br>
+			<a href="${process.env.API_BASE_URL}/auth/activate/${confirmationCode} target="_blank">Ativar minha conta/Confirmar meu email</a>
+			<br>
+			<br>
+			<p>Sua conta não será ativada até que seu email seja confirmado.</p>
+			<br>
+			<p>Se você não se cadastrou no(a) API TEMPLATE recentemente, por favor ignore este email.</p>
+			<br>
+			<p>Obrigado,</p>
+			<p>Equipe API TEMPLATE</p>`,
+		});
+	};
+
+	sendForgotPasswordEmail = async ({ name, email, resetToken }: IEmailService.ForgotPasswordParams): Promise<void> => {
+		await this.send({
+			to: email,
+			subject: 'Redefinir senha',
+			body: `<p>Olá, ${name}</p>
+			<br>
+			<p>Foi solicitada a alteração da sua senha. <a href="${process.env.API_BASE_URL}/auth/reset-password/${resetToken}" target="_blank">Clique aqui</a> para alterá-la (link válido por ${process.env.RESET_PASSWORD_TOKEN_EXPIRATION_IN_MINUTES} minutos).</p>
+			<p>Caso você não tenha solicitado a alteração da senha, por favor desconsidere este email.</p>`,
+		});
+	};
+
+	sendPasswordChangeConfirmationEmail = async ({ name, email }: IEmailService.PasswordChangeConfirmationParams): Promise<void> => {
+		await this.send({
+			to: email,
+			subject: 'A sua senha foi alterada',
+			body: `<p>Olá ${name},</p>
+			<br>
+			<p>A sua senha foi alterada recentemente. Se você não solicitou esta alteração, <a href="#">clique aqui</a> para alterá-la imediatamente.</p>`,
 		});
 	};
 }
