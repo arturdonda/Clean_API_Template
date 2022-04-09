4f95b28fab661fd2b76f58647ee1021ed3251e84
Artur Donda <artur.donda@outlook.com>
2022-04-09 01:48:46 -0300

    Improving UpdatePassword usecase tests

--- src/application/__tests__/user/updatePassword.test.ts
+++ src/application/__tests__/user/updatePassword.test.ts
@@ -42,11 +42,31 @@ describe('Update Password', () => {
 	test('Invalid user', async () => {
 		expect(
 			updatePasswordService.exec({
+				userId: '',
+				password: 'Test@123',
+				confirmationPassword: 'Test@123',
+			})
+		).rejects.toThrow("Campo 'Id' inválido: não pode ser vazio.");
+	});
+
+	test('Nonexistent user', async () => {
+		expect(
+			updatePasswordService.exec({
 				userId: '0',
 				password: 'Test@123',
 				confirmationPassword: 'Test@123',
 			})
 		).rejects.toThrow(UserNotFoundError);
+	});
+
+	test('Invalid password', async () => {
+		expect(
+			updatePasswordService.exec({
+				userId: '1',
+				password: 'TEST@123',
+				confirmationPassword: 'TEST#123',
+			})
+		).rejects.toThrow("Campo 'Senha' inválido: deve conter pelo menos 1 caractere minúsculo (a-z).");
 	});
 
 	test('Unmatch passwords', async () => {
