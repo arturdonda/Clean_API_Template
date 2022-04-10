2b1de39f8fe568375ccee2edcc5f040cb93f49ef
Artur Donda <artur.donda@outlook.com>
2022-04-10 19:46:33 -0300

    Renew access controller checks session cookie

--- src/presentation/controllers/session/renewAccess.ts
+++ src/presentation/controllers/session/renewAccess.ts
@@ -1,12 +1,14 @@
 import { IRenewAccess } from '@domain/usecases/session';
-import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols';
+import { MissingSessionTokenError } from '@presentation/errors';
 import { ok, errorHandler } from '@presentation/helpers';
+import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols';
 
 export class RenewAccessController implements Controller {
 	constructor(private readonly renewAccessService: IRenewAccess) {}
 
 	handle = async (request: HttpRequest): Promise<HttpResponse<null>> => {
 		try {
+			if (!request.cookies?.sessionToken) throw new MissingSessionTokenError();
 			const accessToken = this.renewAccessService.exec(request.cookies.sessionToken);
 
 			return ok({
