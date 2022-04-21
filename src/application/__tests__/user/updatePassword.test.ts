5440f28b85ae3b822a408257918815a2cd10219c
Artur Donda <artur.donda@outlook.com>
2022-04-21 19:24:47 -0300

    Fix updatePassword tests

--- src/application/__tests__/user/updatePassword.test.ts
+++ src/application/__tests__/user/updatePassword.test.ts
@@ -11,7 +11,7 @@ describe('Update Password', () => {
 	const tokenService = new MockTokenService();
 	const ipService = new MockIpService();
 	const createSessionService = new CreateSession(tokenService, ipService);
-	const renewAccessService = new RenewAccess(tokenService, tokenService);
+	const renewAccessService = new RenewAccess(tokenService, tokenService, userRepository);
 	const signInService = new SignIn(userRepository, passwordHashService, createSessionService, renewAccessService);
 
 	test('Valid parameters', async () => {
