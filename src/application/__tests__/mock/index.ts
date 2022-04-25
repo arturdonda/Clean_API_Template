f02adfb83d856060049124d3528da9da986358b7
Artur Donda <artur.donda@outlook.com>
2022-04-24 23:08:00 -0300

    Refactor tests: delete old __test__ folders

--- src/application/__tests__/mock/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './email';
-export * from './hash';
-export * from './ip';
-export * from './token';
-export * from './userRepository';
-export * from './uuid';
--- src/application/__tests__/mock/uuid.ts
+++
@@ -1,8 +0,0 @@
-import { randomUUID } from 'crypto';
-import { IUuidService } from '@application/protocols/utils';
-
-export class MockUuidService implements IUuidService {
-	generate = (seed?: string): string => {
-		return randomUUID().toString().replace(/-/g, '');
-	};
-}
