e36d3f04e0917f1cd57f9a0c4ebd1e63706194ec
Artur Donda <artur.donda@outlook.com>
2022-05-09 01:21:55 -0300

    Express config: test localIp and DB connection

--- tests/main/config/express.spec.ts
+++ tests/main/config/express.spec.ts
@@ -1,11 +1,18 @@
-import app from '@main/config/express';
+import { configureApp } from '@main/config/express';
 import mongo from '@infra/adapters/db/mongoose';
 import request from 'supertest';
+import { setLocalIp } from '@main/helpers';
 
-const agent = request.agent(app);
+jest.mock('@main/helpers/localIp');
+const mongoSpy = jest.spyOn(mongo, 'connect');
 
 describe('Express configuration', () => {
+	let agent: request.SuperAgentTest;
+
 	beforeAll(async () => {
+		const app = await configureApp();
+		agent = request.agent(app);
+
 		await mongo.connect(process.env.MONGO_URL);
 	});
 
@@ -27,5 +34,34 @@ describe('Express configuration', () => {
 			message: expect.any(String),
 			result: expect.anything(),
 		});
+	});
+
+	it('should NOT override ip address', async () => {
+		expect(setLocalIp).toHaveBeenCalledTimes(0);
+	});
+
+	it('should use mock connection string', async () => {
+		expect(mongoSpy).toHaveBeenCalledWith(process.env.MONGO_URL);
+	});
+});
+
+describe('Express dev configuration', () => {
+	let devAgent: request.SuperAgentTest;
+
+	beforeAll(async () => {
+		const app = await configureApp('dev');
+		devAgent = request.agent(app);
+	});
+
+	afterAll(async () => {
+		await mongo.disconnect();
+	});
+
+	it('should override ip address', async () => {
+		expect(setLocalIp).toHaveBeenCalledTimes(1);
+	});
+
+	it('should use real connection string', async () => {
+		expect(mongoSpy).toHaveBeenCalledWith(process.env.MONGODB_CONNECTION_STRING);
 	});
 });
