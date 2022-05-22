576c5533b86d61692e871fef81834fdcb587accf
Artur Donda <artur.donda@outlook.com>
2022-05-22 20:34:57 -0300

    Refactor tests usecases factories

--- tests/_factories/usecases/index.ts
+++ tests/_factories/usecases/index.ts
@@ -1,4 +1,3 @@
-import { IUserRepository } from '@application/protocols/repositories';
 import { CreateSession, RenewAccess, RevokeSession } from '@application/usecases/session';
 import {
 	Activate,
@@ -12,21 +11,19 @@ import {
 	UpdateOptionalData,
 	UpdatePassword,
 } from '@application/usecases/user';
-import { mockEmailService, mockHashService, mockIpService, mockTokenService, mockUuidService } from '@tests/_factories/adapters';
+import { mockEmailService, mockHashService, mockIpService, mockTokenService, mockUuidService, mockUserRepository } from '@tests/_factories/adapters';
 
-export const makeCreateSession = () => new CreateSession(mockTokenService, mockIpService);
-export const makeRenewAccess = (userRepository: IUserRepository) => new RenewAccess(mockTokenService, mockTokenService, userRepository);
-export const makeRevokeSession = (userRepository: IUserRepository) => new RevokeSession(userRepository, mockIpService);
+export const createSessionService = new CreateSession(mockTokenService, mockIpService);
+export const renewAccessService = new RenewAccess(mockTokenService, mockTokenService, mockUserRepository);
+export const revokeSessionService = new RevokeSession(mockUserRepository, mockIpService);
 
-export const makeActivate = (userRepository: IUserRepository) => new Activate(userRepository);
-export const makeForgotPassword = (userRepository: IUserRepository) => new ForgotPassword(userRepository, mockTokenService, mockEmailService);
-export const makeGetActiveSessions = (userRepository: IUserRepository) => new GetActiveSessions(userRepository);
-export const makeGetAllUsers = (userRepository: IUserRepository) => new GetAllUsers(userRepository);
-export const makeGetUserById = (userRepository: IUserRepository) => new GetUserById(userRepository);
-export const makeSignIn = (userRepository: IUserRepository) =>
-	new SignIn(userRepository, mockHashService, makeCreateSession(), makeRenewAccess(userRepository));
-export const makeSignUp = (userRepository: IUserRepository) => new SignUp(userRepository, mockUuidService, mockHashService, mockEmailService);
-export const makeUpdateForgottenPassword = (userRepository: IUserRepository) =>
-	new UpdateForgottenPassword(mockTokenService, makeUpdatePassword(userRepository));
-export const makeUpdateOptionalData = (userRepository: IUserRepository) => new UpdateOptionalData(userRepository);
-export const makeUpdatePassword = (userRepository: IUserRepository) => new UpdatePassword(userRepository, mockHashService, mockEmailService);
+export const activateService = new Activate(mockUserRepository);
+export const forgotPasswordService = new ForgotPassword(mockUserRepository, mockTokenService, mockEmailService);
+export const getActiveSessionsService = new GetActiveSessions(mockUserRepository);
+export const getAllUsersService = new GetAllUsers(mockUserRepository);
+export const getUserByIdService = new GetUserById(mockUserRepository);
+export const signInService = new SignIn(mockUserRepository, mockHashService, createSessionService, renewAccessService);
+export const signUpService = new SignUp(mockUserRepository, mockUuidService, mockHashService, mockEmailService);
+export const updateOptionalDataService = new UpdateOptionalData(mockUserRepository);
+export const updatePasswordService = new UpdatePassword(mockUserRepository, mockHashService, mockEmailService);
+export const updateForgottenPasswordService = new UpdateForgottenPassword(mockTokenService, updatePasswordService);
